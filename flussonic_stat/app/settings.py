0f34147e107fbdd6cf27fee969154b4f31cb4bda
maryandn <maryandnmail@gmail.com>
2022-08-15 04:01:07 +0300

    fix

--- flussonic_stat/app/settings.py
+++ flussonic_stat/app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
