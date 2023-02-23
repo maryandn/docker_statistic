d2b53f491d868f37bc7733166ed83215c656796e
maryandn <maryandnmail@gmail.com>
2023-02-23 17:55:05 +0200

    fix

--- flussonic_stat/monitoring/views.py
+++ flussonic_stat/monitoring/views.py
@@ -35,46 +35,53 @@ class AstraMonitoringView(APIView):
     def post(self, request):
         all_keys = set().union(*(d.keys() for d in request.data))
         ip = get_client_ip(request)
-        if 'channel' in all_keys:
-            response = requests.get(f'http://{ip}:321/playlist.m3u8')
-            res = response.text.splitlines()
-            res.pop(0)
-            res = ''.join(res)[1:].split('#')
-            list_id_astra = []
-            for item in res:
-                item_separator = item.find('http')
-                name_channel = item[10:item_separator]
-                id_channel = item[-15:-11]
-                list_id_astra.append(id_channel)
-                data_item = {
-                    'name_channel': name_channel,
-                    'id_astra': id_channel,
-                    'ip_server': ip
-                }
+        try:
+            if 'channel' in all_keys:
+                response = requests.get(f'http://{ip}:321/playlist.m3u8')
+                res = response.text.splitlines()
+                res.pop(0)
+                res = ''.join(res)[1:].split('#')
+                list_id_astra = []
+                for item in res:
+                    item_separator = item.find('http')
+                    name_channel = item[10:item_separator]
+                    id_channel = item[-15:-11]
+                    list_id_astra.append(id_channel)
+                    data_item = {
+                        'name_channel': name_channel,
+                        'id_astra': id_channel,
+                        'ip_server': ip
+                    }
 
-                ChannelAstraModel.objects.get_or_create(id_astra=id_channel, ip_server=ip, defaults=data_item)
-            qs = ChannelAstraModel.objects.exclude(id_astra__in=list_id_astra).filter(ip_server=ip)
-            qs.delete()
-        elif 'dvb_id' in all_keys:
-            print('++dvb++', all_keys)
-        elif 'onair' in all_keys:
-            keys = ['onair', 'timestamp', 'channel_id']
-            data_keys = [{k: item[k] for k in keys} for item in request.data if item['onair'] == False]
-            if data_keys:
-                get_channel_from_request = list(set([i['channel_id'] for i in data_keys]))
-                qs = ChannelAstraModel.objects.filter(id_astra__in=get_channel_from_request, ip_server=ip)
-                if qs.exists():
-                    for data_key in data_keys:
-                        data_key["channel_id"] = qs.values()[0]['id']
+                    ChannelAstraModel.objects.get_or_create(id_astra=id_channel, ip_server=ip, defaults=data_item)
+                qs = ChannelAstraModel.objects.exclude(id_astra__in=list_id_astra).filter(ip_server=ip)
+                qs.delete()
+            elif 'dvb_id' in all_keys:
+                print('++dvb++', all_keys)
+            elif 'onair' in all_keys:
+                keys = ['onair', 'timestamp', 'channel_id']
+                data_keys = [{k: item[k] for k in keys} for item in request.data if item['onair'] == False]
+                if data_keys:
+                    get_channel_from_request = list(set([i['channel_id'] for i in data_keys]))
+                    qs = ChannelAstraModel.objects.filter(id_astra__in=get_channel_from_request, ip_server=ip)
+                    if qs.exists():
+                        for data_key in data_keys:
+                            data_key["channel_id"] = qs.values()[0]['id']
 
-            serializer = OnAirStatusSerializer(data=data_keys, many=True)
-            if not serializer.is_valid():
-                return Response(serializer.errors)
-            serializer.save()
-        else:
+                serializer = OnAirStatusSerializer(data=data_keys, many=True)
+                if not serializer.is_valid():
+                    return Response(serializer.errors)
+                serializer.save()
+            else:
+                url = f'https://api.telegram.org/{settings.TOKEN_TG}/sendMessage'
+                chat_id = settings.CHAT_ID_TG
+                ip = get_client_ip(request)
+                requests.post(url, data={'chat_id': chat_id, 'text': f'{ip} Incorrect request'})
+        except Exception as e:
+            print(e)
             url = f'https://api.telegram.org/{settings.TOKEN_TG}/sendMessage'
             chat_id = settings.CHAT_ID_TG
             ip = get_client_ip(request)
-            requests.post(url, data={'chat_id': chat_id, 'text': f'{ip} Incorrect request'})
+            requests.post(url, data={'chat_id': chat_id, 'text': f'{ip} Exceptions request'})
 
         return Response(status.HTTP_200_OK)
