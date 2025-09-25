d56550df39565f321e256c934c4162a24a9f5a42
maryandn <maryandnmail@gmail.com>
2025-09-25 20:24:43 +0300

    fix

--- flussonic_stat/core/management/commands/wait_for_db.py
+++ flussonic_stat/core/management/commands/wait_for_db.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
             # os.system(f'mysql -h db03 -u {mysql_user} -p{mysql_password} {database_name} < {backup_file}')
             command = [
                 'mysql', '-h', 'db03', '-u', mysql_user,
-                f'--password={mysql_password}', database_name
+                f'--password={mysql_password}'
             ]
 
             with open(backup_file, 'r') as file:
