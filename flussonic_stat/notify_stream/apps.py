2472f62027009b112db1e7addb0bceb08126d76c
maryandn <maryandnmail@gmail.com>
2025-10-12 16:07:56 +0300

    fix

--- flussonic_stat/notify_stream/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotifyStreamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notify_stream'
--- flussonic_stat/notify_stream/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import StatusStreamView
-
-urlpatterns = [
-    path('', StatusStreamView.as_view()),
-]
