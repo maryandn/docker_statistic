9133de321875a6e8fee783e79bad424a0586a59f
maryandn <maryandnmail@gmail.com>
2025-10-12 21:33:16 +0300

    fix GetStatView

--- flussonic_stat/statistic/views.py
+++ flussonic_stat/statistic/views.py
@@ -1,22 +1,18 @@
-from django.db.models import Sum
 from django.conf import settings
 from django.core.cache import cache
 
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 
 from config.models import ServerModel
 from notify_session.models import StatusSessionModel
-from notify_session.serializers import SessionOpenedSerializer
-from statistic.models import SessionModel
 from statistic.serializers import SessionSerializer
 
 import requests
 from itertools import groupby
-import datetime
 import time
 
 from utils.tg_send_message import send_message_to_tg
@@ -29,52 +25,6 @@ def canonicalize_dict(x):
 def unique_and_count(lst):
     grouper = groupby(sorted(map(canonicalize_dict, lst)))
     return [dict(k + [("count", len(list(g)))]) for k, g in grouper]
-
-
-# class TokenMysqlStatView(generics.ListAPIView):
-#     renderer_classes = [JSONRenderer]
-#     serializer_class = SessionSerializer
-#     permission_classes = [AllowAny]
-#
-#     def get_queryset(self):
-#         base_unix_time = int(datetime.datetime.now().strftime('%s')) // 60 * 60 * 1000
-#         date_list = [base_unix_time - x * 60000 for x in range(24 * 60)]
-#
-#         result = SessionModel.objects.filter(time__in=date_list)
-#         return result
-#
-#     def get(self, request, *args, **kwargs):
-#         base_unix_time = int(datetime.datetime.now().strftime('%s')) // 60 * 60 * 1000
-#         date_list = [base_unix_time - x * 60000 for x in range(24 * 60)]
-#
-#         qs = self.get_queryset()
-#         token_or_ip = kwargs.get('pk')
-#
-#         if token_or_ip.count('.') == 3 and all(0 <= int(num) < 256 for num in token_or_ip.rstrip().split('.')):
-#             result = qs.filter(ip=token_or_ip).values_list('time').annotate(
-#                 count=Sum('count')).order_by('time')
-#
-#             diff = list(set(date_list) - set([i[0] for i in list(result)]))
-#             result = list(result)
-#
-#             for i in diff:
-#                 result.append((i, 0))
-#
-#             result.sort(key=lambda tup: tup[0])
-#
-#         else:
-#             result = qs.filter(token=token_or_ip).values_list('time').annotate(
-#                 count=Sum('count')).order_by('time')
-#
-#             diff = list(set(date_list) - set([i[0] for i in list(result)]))
-#             result = list(result)
-#
-#             for i in diff:
-#                 result.append((i, 0))
-#
-#             result.sort(key=lambda tup: tup[0])
-#
-#         return Response(result, status.HTTP_200_OK)
 
 
 class TokenCacheStatView(APIView):
@@ -110,13 +60,12 @@ class GetStatView(APIView):
 
     def get(self, request, *args, **kwargs):
 
-        base_unix_time = int(datetime.datetime.now().strftime('%s')) // 60 * 60 * 1000
-        date_for_del = base_unix_time - 172800000
+        base_unix_time = int(time.time() // 60 * 60 * 1000)
         list_server = ServerModel.objects.all().values('ip', 'url')
 
-        dict_for_count = []
-        dict_for_deleted = []
-        # dict_all_sessions = []
+        list_all_sessions = []
+        list_for_count = []
+        list_for_deleted = []
 
         for server in list_server:
             ip = server.get('ip')
@@ -128,39 +77,9 @@ class GetStatView(APIView):
                 res.raise_for_status()
                 res = res.json()
                 if res.get('sessions', False):
-                    for i in res.get('sessions'):
-                        if i.get('type') == 'play':
-                            current_unix_time_ms = int(time.time() * 1000)
-                            duration = current_unix_time_ms - i.get('opened_at')
-                            if duration > 60000:
-                                data_dict_sessions = {
-                                    'source': f'{ip}',
-                                    'ip': i.get('ip'),
-                                    'token': i.get('token') if i.get('token') else '',
-                                    'name': i.get('name'),
-                                    'user_id': i.get('user_id') if i.get('user_id') else '',
-                                    'session_id': i.get('id')
-                                }
-                                dict_for_count.append(data_dict_sessions)
-
-                            dict_for_deleted.append(i.get('id'))
-
+                    list_all_sessions.extend(res['sessions'])
                 elif res.get('items', False):
-
-                    for i in res.get('items'):
-                        if i.get('type') == 'play':
-                            if i.get('duration') > 60000:
-                                data_dict_items = {
-                                    'source': f'{ip}',
-                                    'ip': i.get('ip'),
-                                    'token': i.get('token') if i.get('token') else '',
-                                    'name': i.get('name'),
-                                    'user_id': i.get('user_id') if i.get('user_id') else '',
-                                    'session_id': i.get('id')
-                                }
-                                dict_for_count.append(data_dict_items)
-
-                            dict_for_deleted.append(i.get('id'))
+                    list_all_sessions.extend(res['items'])
             except requests.ConnectionError as e:
                 send_message_to_tg(f"Error ConnectionError {ip}: {e}")
                 continue
@@ -171,7 +90,20 @@ class GetStatView(APIView):
                 send_message_to_tg(f"Error RequestException {ip}: {e}")
                 continue
 
-        data = unique_and_count(dict_for_count)
+        filtered_all_sessions = [session for session in list_all_sessions if
+                                 session.get('type') == 'play' and session.get('duration', 0) > 60000]
+
+        for session in filtered_all_sessions:
+            data_dict_items = {
+                'ip': session.get('ip'),
+                'token': session.get('token', ''),
+                'name': session.get('name'),
+                'user_id': session.get('user_id', ''),
+                'session_id': session.get('id')
+            }
+            list_for_count.append(data_dict_items)
+
+        data = unique_and_count(list_for_count)
 
         for item in data:
             token = item.get("token")
@@ -183,46 +115,37 @@ class GetStatView(APIView):
 
             cache.set(key, existing, timeout=172800)
 
-        # serializer = SessionSerializer(data=data, many=True)
-        # if not serializer.is_valid():
-        #     send_message_to_tg(serializer.errors)
-        #     return Response(serializer.errors)
-        # serializer.save()
+        incoming_ids = [item['id'] for item in filtered_all_sessions]
+        existing_ids = set(
+            StatusSessionModel.objects.filter(deleted_at=1, session_id__in=incoming_ids)
+            .values_list('session_id', flat=True))
 
-        # incoming_ids = [item['id'] for item in dict_for_count]
-        # existing_ids = set(
-        #     StatusSessionModel.objects.filter(deleted_at=1, session_id__in=incoming_ids)
-        #     .values_list('session_id', flat=True))
-        #
-        # new_objects = []
-        #
-        # for item in dict_for_count:
-        #     if item['id'] not in existing_ids:
-        #         new_objects.append(StatusSessionModel(
-        #             bytes_sent=item.get('bytes'),
-        #             country=item.get('country'),
-        #             created_at=item.get('opened_at'),
-        #             deleted_at=1,
-        #             ip=item.get('ip'),
-        #             last_access_time=item.get('opened_at'),
-        #             media=item.get('user_name'),
-        #             session_id=item.get('id'),
-        #             token=item.get('token'),
-        #             type='mpegts' if item.get('proto') == 'tshttp' else item.get('proto'),
-        #             user_agent=item.get('user_agent'),
-        #             user_id=item.get('user_id')
-        #         ))
-        #
-        # if new_objects:
-        #     StatusSessionModel.objects.bulk_create(new_objects)
+        new_objects = []
+
+        for item in filtered_all_sessions:
+            if item['id'] not in existing_ids:
+                new_objects.append(StatusSessionModel(
+                    bytes_sent=item.get('bytes'),
+                    country=item.get('country'),
+                    created_at=item.get('opened_at'),
+                    deleted_at=1,
+                    ip=item.get('ip'),
+                    last_access_time=item.get('opened_at'),
+                    media=item.get('user_name'),
+                    session_id=item.get('id'),
+                    token=item.get('token'),
+                    type='mpegts' if item.get('proto') == 'tshttp' else item.get('proto'),
+                    user_agent=item.get('user_agent'),
+                    user_id=item.get('user_id')
+                ))
+
+        if new_objects:
+            StatusSessionModel.objects.bulk_create(new_objects)
 
         no_deleted = list(StatusSessionModel.objects.filter(deleted_at=1).values_list('session_id', flat=True))
 
-        res_for_deleted_at = list(set(no_deleted) - set(dict_for_deleted))
+        res_for_deleted_at = list(set(no_deleted) - set(list_for_deleted))
 
         StatusSessionModel.objects.filter(session_id__in=res_for_deleted_at).update(deleted_at=base_unix_time)
-
-        # session = SessionModel.objects.filter(time__lt=date_for_del)
-        # session.delete()
 
         return Response(status=status.HTTP_200_OK)
