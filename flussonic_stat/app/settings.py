b64a3496b5d23df47c6f146e515e653689d1b33b
maryandn <maryandnmail@gmail.com>
2026-02-13 15:52:32 +0200

    fix

--- flussonic_stat/app/settings.py
+++ flussonic_stat/app/settings.py
@@ -162,7 +162,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
